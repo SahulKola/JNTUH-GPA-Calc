ed8731fef78e77736a57225cf2f2a110f2638491
SahulKola <mail2sahul.js@gmail.com>
2021-12-28 13:46:52 +0530

    Responsive fix

--- app.py
+++ app.py
@@ -340,4 +340,4 @@ def logout():
         return redirect(url_for('home'))
 #--------------------------------------------------------------
 if(__name__ == '__main__'):
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",debug=True,port=8080)
